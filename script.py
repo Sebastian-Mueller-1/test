12804b71fe2f3cc7ad2c2d589db26bac322b5c80
Sebastian-Mueller-1 <86169499+Sebastian-Mueller-1@users.noreply.github.com>
2022-12-29 17:31:08 +0000

    Update script.py

--- script.py
+++ script.py
@@ -1,8 +1,22 @@
 import matplotlib.pyplot as plt
-x=[1,3,5,7]
-y=[2,4,6,1]
-plt.plot(x,y)
-plt.xlabel('X-axis')
-plt.ylabel('Y-axis')
-plt.title("A simple line graph")
-plt.show()
+import networkx as nx
+
+fig, ax = plt.subplots()
+
+G = nx.Graph()
+nodes_california = ["Los Angeles", "San Diego", "San Francisco"]
+nodes_florida = ["Miami", "Orlando", "Tampa"]
+
+G.add_nodes_from(nodes_california)
+G.add_nodes_from(nodes_florida)
+
+G.add_edge("California", "Los Angeles")
+G.add_edge("California", "San Diego")
+G.add_edge("California", "San Francisco")
+G.add_edge("Florida", "Miami")
+G.add_edge("Florida", "Orlando")
+G.add_edge("Florida", "Tampa")
+
+nx.draw(G, with_labels=True)
+
+fig
