cf006bd51ce947731a923f52972e1ad391779b17
Sebastian-Mueller-1 <86169499+Sebastian-Mueller-1@users.noreply.github.com>
2022-12-30 10:15:35 +0000

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,17 @@
-import pandas
+import matplotlib.pyplot as plt
+import numpy as np
 
-print("hello")
+class Foo():
+  
+  def __init__(self):
+    pass
+  
+  def graph(self):
+    xpoints = np.array([1, 8])
+    ypoints = np.array([3, 10])
+
+    plt.plot(xpoints, ypoints)
+    plt.show()
+
+test = Foo()
+test.graph()
