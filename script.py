a80fbc715b98af0ca82fec7c8faf177b08adb86a
Sebastian-Mueller-1 <86169499+Sebastian-Mueller-1@users.noreply.github.com>
2022-12-30 13:23:35 +0000

    Update script.py

--- script.py
+++ script.py
@@ -13,7 +13,7 @@ class Foo():
     # Call plot() method on the appropriate object
     ax[0].plot(x, np.sin(x))
     ax[1].plot(x, np.cos(x))
-    plt.show()
+    fig.show()
 
 test = Foo()
 test.graph()
